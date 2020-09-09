938dbac1f147b4b8d3696727f2b61cf0e4db3d0a
Allan <allanhsz@outlook.com>
2020-09-09 18:37:06 -0300

    FEATURE: implementando component HEAD para trocar o titulo das paginas

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Head from '../Helper/Head';
 
 const UserStats = () => {
-  return <div>estastisticas</div>;
+  return (
+    <div>
+      <Head title="Estastisticas" />
+      estastisticas
+    </div>
+  );
 };
 
 export default UserStats;
